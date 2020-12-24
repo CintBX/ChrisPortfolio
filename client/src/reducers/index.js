bcdbeeecab2da9002e1f323015368647aa5162ee
Dustin Cintron <D.Cint88@gmail.com>
2020-12-24 10:45:37 -0500

    Project Reducer init

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import commissionReducer from './commissionReducer';
+import projectReducer from './projectReducer';
 import errorReducer from './errorReducer';
 
 export default combineReducers({
   commission: commissionReducer,
+  project: projectReducer,
   error: errorReducer
 });
