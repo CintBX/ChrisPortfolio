b0571b5bcfcd770a60ba152ab5fde283ff5f550f
Dustin Cintron <D.Cint88@gmail.com>
2020-12-24 10:45:24 -0500

    Created Project types and updated list

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,6 +1,8 @@
 // Loading
 export const COMMISSIONS_LOADING = 'COMMISSIONS_LOADING';
 export const COMMISSION_LOADING = 'COMMISSION_LOADING';
+export const PROJECTS_LOADING = 'PROJECTS_LOADING';
+export const PROJECT_LOADING = 'PROJECT_LOADING';
 
 // Commission CRUD
 export const GET_COMMISSIONS = 'GET_COMMISSIONS';
@@ -9,11 +11,23 @@ export const ADD_COMMISSION = 'ADD_COMMISSION';
 export const EDIT_COMMISSION = 'EDIT_COMMISSION';
 export const DELETE_COMMISSION = 'DELETE_COMMISSION';
 
-// Error Handling
-export const GET_ERRORS = 'GET_ERRORS';
-export const CLEAR_ERRORS = 'CLEAR_ERRORS';
-
-// CRUD Errors
+// Commission Failures
 export const ADD_COMMISSION_FAIL = 'ADD_COMMISSION_FAIL';
 export const EDIT_COMMISSION_FAIL = 'EDIT_COMMISSION_FAIL';
 export const DELETE_COMMISSION_FAIL = 'DELETE_COMMISSION_FAIL';
+
+// Project CRUD
+export const GET_PROJECTS = 'GET_PROJECTS';
+export const SHOW_PROJECT = 'SHOW_PROJECT';
+export const ADD_PROJECT = 'ADD_PROJECT';
+export const EDIT_PROJECT = 'EDIT_PROJECT';
+export const DELETE_PROJECT = 'DELETE_PROJECT';
+
+// Project Failures
+export const ADD_PROJECT_FAIL = 'ADD_PROJECT_FAIL';
+export const EDIT_PROJECT_FAIL = 'EDIT_PROJECT_FAIL';
+export const DELETE_PROJECT_FAIL = 'DELETE_PROJECT_FAIL';
+
+// Error Handling
+export const GET_ERRORS = 'GET_ERRORS';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS';
