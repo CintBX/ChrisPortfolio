b21cd612775298375c3c8fcaed2246791f5de0d8
Dustin Cintron <D.Cint88@gmail.com>
2020-12-26 15:57:38 -0500

    Footer buttons now adjust to screensize

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,49 +1,58 @@
 import React, { Component } from 'react';
 import { MDBIcon, MDBContainer, MDBBtn } from 'mdbreact';
 import { Link } from 'react-router-dom';
+import { Row, Col } from 'reactstrap';
 
 class Footer extends Component {
   render() {
     return (
-      <div>
-        <MDBContainer style={styles.container}>
-          <Link to="/contact-me" style={styles.link}>
-            <MDBBtn size="lg" social="email" color="info" style={styles.button}>
-              <MDBIcon icon="envelope" className="pr-1" style={styles.icon} /> Email Me
-            </MDBBtn>
-          </Link>
+      <MDBContainer style={styles.container}>
+          <Row>
+          <Col sm="auto">
+            <Link to="/contact-me" style={styles.link}>
+              <MDBBtn size="lg" social="email" color="info">
+                <MDBIcon icon="envelope" className="pr-1" style={styles.icon} /> Email Me
+              </MDBBtn>
+            </Link>
+          </Col>
 
-          <a href="https://www.facebook.com/chris.d.3150"
-            target="_blank"
-            ref="noreferrer noopener"
-            style={styles.link}
-          >
-            <MDBBtn size="lg" social="fb" color="blue" style={styles.button}>
-              <MDBIcon fab icon="facebook-f" className="pr-1" style={styles.icon} /> Facebook
-            </MDBBtn>
-          </a>
+          <Col sm="auto">
+            <a href="https://www.facebook.com/chris.d.3150"
+              target="_blank"
+              ref="noreferrer noopener"
+              style={styles.link}
+            >
+              <MDBBtn size="lg" social="fb" color="blue">
+                <MDBIcon fab icon="facebook-f" className="pr-1" style={styles.icon} /> Facebook
+              </MDBBtn>
+            </a>
+          </Col>
   
-          <a href="https://www.instagram.com/virtuoso_lume/"
-            target="_blank"
-            ref="noreferrer noopener"
-            style={styles.link}
-          >
-            <MDBBtn size="lg" social="ins" className="peach-gradient" style={styles.button}>
-              <MDBIcon fab icon="instagram" className="pr-1" style={styles.icon} /> Instagram
-            </MDBBtn>
-          </a>
+          <Col sm="auto">
+            <a href="https://www.instagram.com/virtuoso_lume/"
+              target="_blank"
+              ref="noreferrer noopener"
+              style={styles.link}
+            >
+              <MDBBtn size="lg" social="ins" className="peach-gradient">
+                <MDBIcon fab icon="instagram" className="pr-1" style={styles.icon} /> Instagram
+              </MDBBtn>
+            </a>
+          </Col>
   
-          <a href="https://www.youtube.com/channel/UCj8pYnnOtWp5OKHzVOh0R3A"
-            target="_blank"
-            ref="noreferrer noopener"
-            style={styles.link}
-          >
-            <MDBBtn size="lg" social="yt" color="red" style={styles.button}>
-              <MDBIcon fab icon="youtube" className="pr-1" style={styles.icon} /> Youtube
-            </MDBBtn>
-          </a>
-        </MDBContainer>
-      </div>
+          <Col sm="auto">
+            <a href="https://www.youtube.com/channel/UCj8pYnnOtWp5OKHzVOh0R3A"
+              target="_blank"
+              ref="noreferrer noopener"
+              style={styles.link}
+            >
+              <MDBBtn size="lg" social="yt" color="red">
+                <MDBIcon fab icon="youtube" className="pr-1" style={styles.icon} /> Youtube
+              </MDBBtn>
+            </a>
+          </Col>
+        </Row>
+      </MDBContainer>
     );
   };
 };
@@ -52,11 +61,8 @@ const styles = {
   container: {
     display: 'flex',
     justifyContent: 'center',
-    padding: 50
-  },
-  button: {
-    marginLeft: 15,
-    marginRight: 15
+    paddingTop: 25,
+    paddingBottom: 50
   },
   icon: {
     fontSize: '1.5em',
