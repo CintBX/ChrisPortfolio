1a44be2253909dcb6ce8c152fc17c461cd38ba55
Dustin Cintron <D.Cint88@gmail.com>
2020-12-26 16:32:15 -0500

    Converted show comp to class to fix bug

--- client/src/components/commission/show.js
+++ client/src/components/commission/show.js
@@ -1,53 +1,59 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { Component } from 'react';
 import { showCommission } from '../../actions/commissionActions';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { Container, Row, Col, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-const CommissionShowPage = props => {
-  const dispatch = useDispatch();
-  const { commission, loading } = useSelector(
-    state => ({
-      commission: state.commission.showCommission,
-      loading: state.commission.loading
-    })
-  );
+class CommissionShowPage extends Component {
+  constructor(props) {
+    super(props);
+  };
 
-  useEffect(() => {
-    const id = props.match.params.id;
-    dispatch(showCommission(id));
-  });
+  static propTypes = {
+    showCommission: PropTypes.func.isRequired,
+    commission: PropTypes.object.isRequired
+  };
 
-  if(loading) {
-    return <h1>This commission is loading.  Please wait..</h1>
-  } else {
-    return (
-      <div style={styles.container}>
-        <Container>
-          <Row>
-            <Col lg={5} xl={5}>
-              <img
-                src={ commission.image ? commission.image : "https://via.placeholder.com/325" }
-                alt="Commission Display"
-              />
-            </Col>
-            <Col lg={7} xl={7}>
-              <h1><span style={styles.softenTone}>Title: </span>{ commission.title }</h1>
-              <h3><span style={styles.softenTone}>Sold for: </span>${ commission.price }</h3>
-              <h3><span style={styles.softenTone}>About this piece: </span></h3>
-              <p>
-                { commission.description }
-              </p>
-              <br/>
-              <Link to={`/edit-commission/${commission._id}`}>
-                <Button style={styles.editButton} outline color="warning">Edit</Button>
-              </Link>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    )
-  }
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    this.props.showCommission(id);
+  };
+
+  render() {
+    const { _id, image, title, price, description } = this.props.commission.showCommission;
+    const { loading } = this.props.commission;
+    if(loading) {
+      return <h1>This commission is loading.  Please wait..</h1>
+    } else {
+      return (
+        <div style={styles.container}>
+          <Container>
+            <Row>
+              <Col lg={5} xl={5}>
+                <img
+                  src={ image ? image : "https://via.placeholder.com/325" }
+                  alt="Commission Display"
+                />
+              </Col>
+              <Col lg={7} xl={7}>
+                <h1><span style={styles.softenTone}>Title: </span>{ title }</h1>
+                <h3><span style={styles.softenTone}>Sold for: </span>${ price }</h3>
+                <h3><span style={styles.softenTone}>About this piece: </span></h3>
+                <p>
+                  { description }
+                </p>
+                <br/>
+                <Link to={`/edit-commission/${_id}`}>
+                  <Button style={styles.editButton} outline color="warning">Edit</Button>
+                </Link>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      )
+    }
+  };
 };
 
 const styles = {
@@ -62,6 +68,10 @@ const styles = {
   softenTone: {
     color: "lightgrey"
   }
-}
+};
 
-export default CommissionShowPage;
+const mapStateToProps = state => ({
+  commission: state.commission
+});
+
+export default connect(mapStateToProps, { showCommission })(CommissionShowPage);
