5b887b7838ca0c0ef990538c706543e82db92dea
Dustin Cintron <D.Cint88@gmail.com>
2020-12-28 13:15:26 -0500

    Created User types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,13 @@
+// User Auth
+export const USER_LOADING = 'USER_LOADING';
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+
 // Loading
 export const COMMISSIONS_LOADING = 'COMMISSIONS_LOADING';
 export const COMMISSION_LOADING = 'COMMISSION_LOADING';
@@ -10,20 +20,16 @@ export const SHOW_COMMISSION = 'SHOW_COMMISSION';
 export const ADD_COMMISSION = 'ADD_COMMISSION';
 export const EDIT_COMMISSION = 'EDIT_COMMISSION';
 export const DELETE_COMMISSION = 'DELETE_COMMISSION';
-
-// Commission Failures
 export const ADD_COMMISSION_FAIL = 'ADD_COMMISSION_FAIL';
 export const EDIT_COMMISSION_FAIL = 'EDIT_COMMISSION_FAIL';
 export const DELETE_COMMISSION_FAIL = 'DELETE_COMMISSION_FAIL';
 
-// Project CRUD
+// Project CRUD (Inactive)
 export const GET_PROJECTS = 'GET_PROJECTS';
 export const SHOW_PROJECT = 'SHOW_PROJECT';
 export const ADD_PROJECT = 'ADD_PROJECT';
 export const EDIT_PROJECT = 'EDIT_PROJECT';
 export const DELETE_PROJECT = 'DELETE_PROJECT';
-
-// Project Failures
 export const ADD_PROJECT_FAIL = 'ADD_PROJECT_FAIL';
 export const EDIT_PROJECT_FAIL = 'EDIT_PROJECT_FAIL';
 export const DELETE_PROJECT_FAIL = 'DELETE_PROJECT_FAIL';
