17d4d86dca8b25610c61ae480090851c3963f018
Dustin Cintron <D.Cint88@gmail.com>
2020-12-28 13:15:38 -0500

    Import new user reducer to reducer index

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import commissionReducer from './commissionReducer';
 import projectReducer from './projectReducer';
 import errorReducer from './errorReducer';
+import userReducer from './userReducer';
 
 export default combineReducers({
   commission: commissionReducer,
   project: projectReducer,
-  error: errorReducer
+  error: errorReducer,
+  user: userReducer
 });
