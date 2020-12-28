c4eb83d01df0270e0f740b34d83727d9ef3618c8
Dustin Cintron <D.Cint88@gmail.com>
2020-12-28 13:38:08 -0500

    user actions and reducer

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -10,7 +10,7 @@ import {
 } from '../actions/types';
 
 const initialState = {
-  token: localStorage.getItem('token');
+  token: localStorage.getItem('token'),
   isAuthenticated: false,
   isLoading: false,
   user: null
