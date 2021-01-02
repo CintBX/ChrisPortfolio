9d8abd998d14928287b8a47741b55fa578a74fde
Dustin Cintron <D.Cint88@gmail.com>
2021-01-02 12:53:51 -0500

    Refactored multer upload into /middleware

--- routes/api/commissions.js
+++ routes/api/commissions.js
@@ -2,34 +2,10 @@ const express = require('express');
 const router = express.Router();
 const paginate = require('jw-paginate');
 const authorize = require('../../middleware/authorize');
-const multer = require('multer');
+const upload = require('../../middleware/upload');
 
 // Commission model
 const Commission = require('../../models/Commission');
-
-// Image/multer resources
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, './uploads/');
-  },
-  filename: function(req, file, cb) {
-    cb(null, Date.now() + file.originalname);
-  }
-});
-const fileFilter = (req, file, cb) => {
-  if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png') {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  };
-};
-const upload = multer({
-  storage: storage,
-  limits: {
-    fileSize: 1024 * 1024 * 5
-  },
-  fileFilter: fileFilter
-});
 
 // @route   POST /commissions
 // @descrip Create a new commission
