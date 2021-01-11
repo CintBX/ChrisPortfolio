c1cda6d83632852ad06db816481b6004713e27a4
Dustin Cintron <D.Cint88@gmail.com>
2021-01-10 19:02:00 -0500

    Commented out devtools code for lulz

--- client/src/store.js
+++ client/src/store.js
@@ -6,8 +6,8 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : null
-const devTools2 = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
+// const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : null
+// const devTools2 = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() : null
 
 const store = createStore(
   rootReducer,
