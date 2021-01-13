df048d79082195b91dfde412775976867fcbbd64
Dustin Cintron <D.Cint88@gmail.com>
2021-01-13 13:23:15 -0500

    Server Deployment code adjust again

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ mongoose
   .catch(err => console.log(err));
 
 
-// For Heroku deployment
+// For Deployment
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'public')));
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'public', 'index.html'));
   });
