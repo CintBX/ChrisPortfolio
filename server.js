8a505b91847db2bd3c973bd476f64afb51ddfbaf
Dustin Cintron <D.Cint88@gmail.com>
2021-01-13 13:19:37 -0500

    client/public/index for Production (not 'build')

--- server.js
+++ server.js
@@ -38,9 +38,9 @@ mongoose
 
 // For Heroku deployment
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  app.use(express.static(path.join(__dirname, 'client', 'public')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'public', 'index.html'));
   });
 };
 
