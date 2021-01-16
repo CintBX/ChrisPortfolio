0dddbb2edd74fb2792dd06b0b49438f2ebbb9ccf
Dustin Cintron <D.Cint88@gmail.com>
2021-01-16 14:31:37 -0500

    Increased lifespan of login token

--- routes/api/users.js
+++ routes/api/users.js
@@ -75,7 +75,7 @@ router.post('/login', (req, res) => {
           jwt.sign(
             { id: user.id },
             process.env.JWT_SECRET,
-            { expiresIn: 3600 },
+            { expiresIn: 43200 },
             (err, token) => {
               if(err) throw err;
               res.json({
