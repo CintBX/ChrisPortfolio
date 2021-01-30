5602000965e0a1a8bcd69e0576a08df73b43464b
Dustin Cintron <D.Cint88@gmail.com>
2021-01-30 16:06:04 -0500

    New and Improved store for data persist

--- client/src/store.js
+++ client/src/store.js
@@ -2,17 +2,27 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-const initialState = {};
+function saveToLocalStorage(state) {
+  const serializedState = JSON.stringify(state);
+  localStorage.setItem('state', serializedState);
+};
 
-const middleware = [thunk];
+function loadFromLocalStorage() {
+  const serializedState = localStorage.getItem('state');
+  if (serializedState === null) return undefined;
+  return JSON.parse(serializedState);
+};
+
+const persistedState = loadFromLocalStorage();
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware)
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  persistedState,
+  composeEnhancers(applyMiddleware(thunk)),
 );
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 export default store;
